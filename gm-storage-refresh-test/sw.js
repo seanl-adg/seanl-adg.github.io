e2b7ea98c8cc4a5386d82395c843f462d1e3d344
seanl-adg <seanl@adguard.com>
2018-06-19 23:29:59 +0900

    Test sw

--- gm-storage-refresh-test/sw.js
+++ gm-storage-refresh-test/sw.js
@@ -2,8 +2,10 @@ addEventListener('fetch', event => {
     console.log(`fetch event for url: ${event.request.url}`);
     let requestUrl = new URL(event.request.url);
     if (
-        requestUrl.host === 'local.adguard.com' ||
-        requestUrl.host === 'injections.adguard.com'
+        (requestUrl.host === 'local.adguard.com' ||
+        requestUrl.host === 'injections.adguard.com') &&
+        requestUrl.pathname.startsWith('/adguard-ajax-api/injections/')
+
     ) {
         event.respondWith(fetch(event.request, { cache: "no-cache" }));
     }
